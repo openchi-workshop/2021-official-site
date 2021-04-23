e507cab4e14ba4e82e007323a07ec79310aa45d7
Yeh-Jung, Tsai <71741526+Emily0509@users.noreply.github.com>
2021-04-23 16:41:19 +0800

    contact

---
+++ src/components/Title.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="title">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    text: { type: String, required: true },
+  },
+};
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+.title {
+    font-family: "Arvo", serif;
+    color:#FFFFFF;
+    font-style: bold;
+    font-size: 18px;
+    line-height: 22px;
+    align-items: left;
+    vertical-align: center;
+}
+</style>
