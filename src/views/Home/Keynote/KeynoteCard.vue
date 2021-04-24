2ba59dc2d3129394750c222fd5dcfec9ec441389
Jeffery <jefferyhsieh1996@gmail.com>
2021-04-24 21:52:18 +0800

    feat: adjust keynote card layout

--- src/views/Home/Keynote/KeynoteCard.vue
+++ src/views/Home/Keynote/KeynoteCard.vue
@@ -1,29 +1,21 @@
 <template>
   <StyledBox>
-    <div no-body>
-      <b-row no-gutters class="keynote-card">
-        <b-col md="4">
-          <b-card-img
-            :src="require(`@/assets/${img}`)"
-            alt="Avator"
-            class="rounded-0 keynote-card__img"
-          ></b-card-img>
-        </b-col>
-        <b-col md="8" class="keynote-card__body">
-          <b-card-body>
-            <b-card-text class="keynote-card__title">
-              {{ name }}
-              <span class="keynote-card__title--work">{{ work }}</span>
-            </b-card-text>
-            <b-card-text class="keynote-card__body--title">
-              {{ title }}
-            </b-card-text>
-            <b-card-text class="keynote-card__body--description">
-              {{ description }}
-            </b-card-text>
-          </b-card-body>
-        </b-col>
-      </b-row>
+    <div no-gutters class="keynote-card">
+      <img
+        :src="require(`@/assets/${img}`)"
+        alt="Avatar"
+        class="rounded-0 keynote-card__img"
+      />
+      <div class="keynote-card__body">
+        <div class="keynote-card__title">
+          <div class="keynote-card__title--name">&lt; {{ name }} &gt;</div>
+          <div class="keynote-card__title--work">{{ work }}</div>
+        </div>
+        <div class="keynote-card__body--title">&lt; {{ title }} &gt;</div>
+        <div class="keynote-card__body--description">
+          {{ description }}
+        </div>
+      </div>
     </div>
   </StyledBox>
 </template>
@@ -50,32 +42,43 @@ export default {
 .keynote-card {
   font-family: "Noto Sans TC", sans-serif;
   color: #fff;
-  height: 200px;
   width: 680px;
   background: #1c1c24;
+  display: flex;
+  flex-direction: row;
+
   &__img {
     width: 200px;
+    height: 200px;
   }
+
   &__title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     font-weight: 700;
     font-size: 24px;
     line-height: 35px;
     margin-bottom: 11px;
+
     &--work {
       margin-left: 7px;
-
       font-weight: 400;
       font-size: 14px;
       line-height: 20px;
+      color: #00ff00;
     }
   }
+
   &__body {
-    margin-left: -20px;
+    margin: 8px 24px;
+
     &--title {
       font-size: 18px;
       line-height: 26px;
       margin-bottom: 8px;
     }
+
     &--description {
       font-size: 14px;
       line-height: 20px;
--- src/views/Home/Keynote/index.vue
+++ src/views/Home/Keynote/index.vue
@@ -2,61 +2,18 @@
   <div id="keynote">
     <Title text="# KEYNOTE SPEAKER" class="keynote__title" />
     <div class="keynote__list">
-      <b-row>
-        <b-col align-self="start" class="keynote__card">
-          <KeynoteCard
-            img="講者1.jpeg"
-            name="< 林蕙如 >"
-            work="悠識數位 研究總監"
-            title="< 規劃到轉化，如何進行使用者研究 >"
-            description="使用者研究怎麼進行，該考量的事情有哪些？這一個小時我們將濃縮幾年的經驗，跟各位聊聊研究在實際上執行的各種狀況以及眉眉角角，從擬定計畫，定義族群，到研究執行，收斂報告，讓大家一窺使用者研究的起承轉合。"
-          />
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col align-self="start" offset-md="1" class="keynote__card">
-          <KeynoteCard
-            img="講者2.jpeg"
-            name="< 講者B>"
-            work=" 講者B工作單位 "
-            title="< 講者B演講主題 >"
-            description="演講主題簡介 "
-          />
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col align-self="start" offset-md="2" class="keynote__card">
-          <KeynoteCard
-            img="講者3.jpeg"
-            name="< 陳威帆 >"
-            work=" Fourdesire 創辦人兼製作人 "
-            title="< 講者C演講主題 >"
-            description="演講主題簡介 "
-          />
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col align-self="start" offset-md="3" class="keynote__card">
-          <KeynoteCard
-            img="講者4.jpeg"
-            name="< 洪燕茹 >"
-            work=" 5% Design Action 社會設計平台 共同發起人 "
-            title="< 講者D演講主題 >"
-            description="演講主題簡介 "
-          />
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col align-self="start" offset-md="4" class="keynote__card">
-          <KeynoteCard
-            img="講者5.jpeg"
-            name="< 講者E>"
-            work=" 講者E工作單位 "
-            title="< 講者E演講主題 >"
-            description="演講主題簡介 "
-          />
-        </b-col>
-      </b-row>
+      <template v-for="(speaker, i) in speakers">
+        <b-row :key="speaker.name">
+          <b-col align-self="start" :offset-md="i" class="keynote__card">
+            <template v-if="speaker.name">
+              <keynote-card v-bind="speaker" />
+            </template>
+            <template v-else>
+              <keynote-blank-card />
+            </template>
+          </b-col>
+        </b-row>
+      </template>
     </div>
   </div>
 </template>
@@ -65,12 +22,46 @@
 // @ is an alias to /src
 import Title from "@/components/ui/Title";
 import KeynoteCard from "./KeynoteCard";
+import KeynoteBlankCard from "./KeynoteBlankCard";
 
 export default {
   name: "Keynote",
   components: {
     Title,
     KeynoteCard,
+    KeynoteBlankCard,
+  },
+  data() {
+    return {
+      speakers: [
+        {
+          img: "speaker_1.jpeg",
+          name: "林蕙如",
+          work: "悠識數位 研究總監",
+          title: "規劃到轉化，如何進行使用者研究",
+          description:
+            "使用者研究怎麼進行，該考量的事情有哪些？這一個小時我們將濃縮幾年的經驗，跟各位聊聊研究在實際上執行的各種狀況以及眉眉角角，從擬定計畫，定義族群，到研究執行，收斂報告，讓大家一窺使用者研究的起承轉合。",
+        },
+        {
+          img: "speaker_2.jpeg",
+          name: "陳威帆",
+          work: "Fourdesire 創辦人兼製作人",
+          title: "敬請期待演講主題",
+          description:
+            "陳威帆是一名軟體開發者、屢獲殊榮的遊戲設計師和製作人。 他創立了 Fourdesire 旨在製作各種有意義且富饒趣味的遊戲產品，為社會帶來積極影響。他的眾多獲獎作品如《植物保姆》、《Walkr》和《記帳城市》使用者已破全球數千萬，並受到華盛頓郵報、商業內幕、IGN、Polygon 等媒體報導。",
+        },
+        {
+          img: "speaker_3.jpeg",
+          name: "洪燕茹",
+          work: "5% Design Action 社會設計平台 共同發起人",
+          title: "敬請期待演講主題",
+          description:
+            "職游-職涯詢工作室 共同創辦人、GCDF國際生涯發展 諮詢師、台北護理大學 職涯探勘課程 講師、TEDX年會及沙龍 受邀講者、多場國際型跨域創新工作坊 計畫PM，帶領演講與工作坊超過180場。擅長人生設計，社會/服務設計，以及跨領域共創。",
+        },
+        {},
+        {},
+      ],
+    };
   },
 };
 </script>
